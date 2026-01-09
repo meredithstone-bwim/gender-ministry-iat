e3a5f151ca49226a2a11a0db256b75abe52043a3
meredithstone-bwim <meredithstone@bwim.info>
2026-01-09 09:03:09 -0600

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -45,11 +45,11 @@ define(['questAPI'], function(Quest){
     API.addQuestionsSet('therm',{
         inherit: 'basicSelect',
         answers: [
-            {text:'5 - Strongly affirming', value:5},
-            {text:'4 - Affirming', value:4},
-            {text:'3 - Neutral', value:3},
-            {text:'2 - Not affirming', value:2},
-            {text:'1 - Strongly not affirming', value:1}
+            {text:'Strongly affirming', value:5},
+            {text:'Affirming', value:4},
+            {text:'Neutral', value:3},
+            {text:'Not affirming', value:2},
+            {text:'Strongly not affirming', value:1}
         ]
     });
 
@@ -60,22 +60,20 @@ define(['questAPI'], function(Quest){
     API.addQuestionsSet('attributes7',{
         inherit : 'basicSelect',
         name: 'attributes7',
-        stem: 'Which statement best describes you?',
+        stem: 'How would you define your personal position on women in ministry?',
         answers: [
-            {text:'I strongly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:7},
-            {text:'I moderately prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:6},
-            {text:'I slightly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:5},
-            {text:'I like <%= global.whiteLabels %> and <%= global.blackLabels %> equally.',value:4},
-            {text:'I slightly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:3},
-            {text:'I moderately prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:2},
-            {text:'I strongly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:1}
+            {text:'Strongly affirming', value:5},
+            {text:'Affirming',value:4},
+            {text:'Neutral',value:3},
+            {text:'Not affirming',value:2},
+            {text:'Strongly not affirming',value:1}
         ]
     });
 	
     API.addQuestionsSet('thermBlack',{
         inherit : 'therm',
         name: 'Tblack_0to10',
-        stem: 'How would you categorize the position of your church toward women in ministry?'
+        stem: 'How would you define the position of your church on women in ministry?'
     });
 
        API.addSequence([
